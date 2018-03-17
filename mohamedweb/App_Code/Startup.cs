a396b0c2bfabd0a5537f3f58a619a1bfc2cb5948
hananfci <hananfci2014@gmail.com>
2018-03-17 15:32:43 +0200

    this is last updat

---
+++ mohamedweb/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mohamedweb.Startup))]
+namespace mohamedweb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
